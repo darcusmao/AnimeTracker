dff26630ecda2659f13e57de1478d0af78e8da0d
Marcus Dao <marcusdao3412@gmail.com>
2023-11-09 22:50:43 -0700

    Implemented some backend to store anime

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, onMounted} from 'vue'
+
+const query = ref('')
+const my_anime = ref([])
+const search_results = ref([])
+
+const my_anime_asc = computed(() => { 
+  return my_anime.value.sort((a,b) => {
+    return a.title.localCompare(b.title)
+  })
+})
+
+const searchAnime = () => {
+  const url = 'https://api.jikan.moe/v4/anime?q=${query.value}'
+  fetch(url)
+    .then(res=>res.json())
+    .then(res=> {
+      search_results.value = res.data
+    })
+  const handleInput = e => {
+    if (!e.target.value) {
+      search_results.value = []
+    }
+  }
+}
+
+const addAnime = anime => {
+  search_results.value = []
+  query.value = ''
+
+  my_anime.value.push({
+    id: anime.mal_id,
+    title: anime.title,
+    image: anime.images.jpg.image_url,
+    total_episdoes: anime.episodes,
+    watched_episodes: 0
+  })
+
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+}
+
+const increaseWatch = anime=> {
+  anime.watched_episodes++
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+}
+
+const decreasewatch = anime=> {
+  anime.watched_episodes--
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+}
+
+onMounted(() => {
+  my_anime.value = JSON.parse(localStorage.getItem('my_anime')) || []
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1>My Anime Tracker</h1>
+    
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
